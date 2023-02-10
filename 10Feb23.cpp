80cfb48aaf8ada4f080a4cfb200b4f5f68b5c594
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-10 23:36:58 +0530

    gfg

---
+++ 10Feb23.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxInstance(string s){
+        int b=0, a=0, l=0, o=0, n=0;
+        int ans;
+        
+        for (int i=0; i<s.length(); i++)
+        {
+            if (s[i]=='b')
+            {
+                b++;
+            }
+            else if (s[i]=='a')
+            {
+                a++;
+            }
+            else if (s[i]=='l')
+            {
+                l++;
+            }
+            else if (s[i]=='o')
+            {
+                o++;
+            }
+            else if (s[i]=='n')
+            {
+                n++;
+            }
+        }
+        
+        if (l%2==0)
+        {
+            l=l/2;
+        }
+        else
+        {
+            l=(l-1)/2;
+        }
+        
+        if (o%2==0)
+        {
+            o=o/2;
+        }
+        else
+        {
+            o=(o-1)/2;
+        }
+        
+        ans=min(b, a);
+        ans=min(ans, l);
+        ans=min(ans, o);
+        ans=min(ans, n);
+    
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.maxInstance(s)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
