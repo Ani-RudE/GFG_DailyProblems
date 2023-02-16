81cbf2d9eaa6796bb6a063d50aa175e8d4af85f8
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-16 16:31:41 +0530

    gfg

---
+++ 16Feb23.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+    int goodStones(int n, int a[]){
+        // Code here
+        int i=0;
+     while (i<7 && i>=0)
+     {
+          
+     }
+        
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t=1;
+//     cin>>t;
+    while(t--)
+    {
+        int n=7;
+     //    cin>>n;
+        int arr[]={2,3,-1,2,-2,4,1};
+     //    for(int i=0;i<n;i++){
+     //        cin>>arr[i];
+     //    }
+     Solution ob;
+     cout<<ob.goodStones(n,arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
