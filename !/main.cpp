4f7959a48ee76e175621acf22ad24f80c3c26b94
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-16 16:19:25 +0530

    gfg

---
+++ !/main.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+    int func(int n,vector<int> &arr){
+        // Code here
+        
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.func(n,arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
