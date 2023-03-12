12806da212bc6cf0f66e642a74dffb60cebb99c1
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-12 19:11:30 +0530

    potd

---
+++ 12Mar2023.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     int n=3;
+     int m[n][n]={1, 1, 1, 
+                  1, 1, 1, 
+                  0, 0, 0};
+
+     return 0;
+}
